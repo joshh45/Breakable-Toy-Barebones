159f754de3bddd5fa5971c27bd176b87595f9a2c
Josh Ho <hojo1025@gmail.com>
2017-02-05 23:17:18 -0500

    user model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:name) { |n| "Josh Man #{n}" }
+    sequence(:email) { |e| "football#{e}@gmail.com" }
+    password "ballislife"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it { should have_valid(:name).when('Josh', 'Patriot') }
+  it { should_not have_valid(:name).when(nil, "") }
+
+  it { should have_valid(:email).when('josh@gmail.com', 'baller@pats.com') }
+  it { should_not have_valid(:email).when(nil, '') }
+
+  it 'has a matching password confirmation for the password' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 end
